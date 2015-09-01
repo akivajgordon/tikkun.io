cfd72cf1ba2678d17062792fb90a49f9c85d9a2d
Akiva Gordon <akivag29@gmail.com>
2015-08-31 23:41:22 -0400

    Create a Tikkun Line Breaker.

--- src/tikkun.js
+++ src/tikkun.js
@@ -62,3 +62,24 @@
 
     window.TikkunColumn = TikkunColumn;
 }());
+
+(function () {
+    "use strict";
+
+    function TikkunLineBreaker() {
+    }
+
+    TikkunLineBreaker.prototype.textFromVerses = function (verses, lineStarts) {
+
+        var thisLine = lineStarts[0],
+            nextLine = lineStarts[1],
+            includedVerses = verses.slice(thisLine.v - 1, nextLine.v),
+            allWords = includedVerses.join(" ").split(" "),
+            lastVerseWordCount = verses[nextLine.v - 1].split(" ").length,
+            trimmedWords = allWords.slice(thisLine.w - 1, allWords.length - (lastVerseWordCount - nextLine.w + 1));
+
+        return trimmedWords.join(" ");
+    };
+
+    window.TikkunLineBreaker = TikkunLineBreaker;
+}());

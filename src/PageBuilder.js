fc2c9cc4d60bec0299d5ed96c1e4297f42fe83df
Akiva Gordon <akivag29@gmail.com>
2015-10-13 01:21:33 -0400

    Abstract away the details of finding the next line.

--- src/PageBuilder.js
+++ src/PageBuilder.js
@@ -5,6 +5,30 @@
 
     var newPage = Tikkun.Page;
 
+    function isLastLineOfColumn(lineIndex, columnIndex, arrangement) {
+        return lineIndex >= arrangement[columnIndex].length - 1;
+    }
+
+    function isLastColumn(columnIndex, arrangement) {
+        return columnIndex >= arrangement.length - 1;
+    }
+
+    function firstLineOfNextColumn(columnIndex, arrangement) {
+        if (!isLastColumn(columnIndex, arrangement)) {
+            return arrangement[columnIndex + 1][0];
+        }
+
+        return null;
+    }
+
+    function lineAfterLineInColumn(lineIndex, columnIndex, arrangement) {
+        if (isLastLineOfColumn(lineIndex, columnIndex, arrangement)) {
+            return firstLineOfNextColumn(columnIndex, arrangement);
+        }
+
+        return arrangement[columnIndex][lineIndex + 1];
+    }
+
     function newPageBuilder(spec) {
 
         var arrangement = spec.arrangement,
@@ -14,9 +38,8 @@
             newLineBuilder = spec.newLineBuilder,
             pageAtIndex = function (pageIndex) {
                 var thisColumn = arrangement[pageIndex] || [],
-                    nextColumn = arrangement[pageIndex + 1] || [],
-                    lines = thisColumn.map(function (lineStart, lineIndex, column) {
-                        var nextLine = lineIndex + 1 < column.length ? column[lineIndex + 1] : nextColumn[0],
+                    lines = thisColumn.map(function (lineStart, lineIndex) {
+                        var nextLine = lineAfterLineInColumn(lineIndex, pageIndex, arrangement),
                             lineBuilder = newLineBuilder(torahText, aliyot, lineStart, nextLine);
 
                         return newLine({

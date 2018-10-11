4caf2271e3b8ceeb943d47ae9b445dc12aebccd3
Akiva Gordon <akivag29@gmail.com>
2018-10-10 22:52:44 -0400

    Put each sefer in columns in the parsha picker

--- components/ParshaPicker.js
+++ components/ParshaPicker.js
@@ -1,15 +1,37 @@
 import parshiyot from '../build/parshiyot.json'
 
+const Parsha = ({ ref, he }) => `<li
+  class="parsha"
+  data-target-id="parsha"
+  data-jump-to-book="${ref.b}"
+  data-jump-to-chapter="${ref.c}"
+  data-jump-to-verse="${ref.v}"
+>
+  ${he}
+</li>
+`
+
+const Book = (book) => `
+  <li class="parsha-book">
+    <ol class="parsha-list">
+      ${book.map(Parsha).join('')}
+    </ol>
+  </li>
+`
+
 const ParshaPicker = () => `
   <div class="parsha-picker">
-    <ol class="parsha-list">
-      ${parshiyot.map(({ he, ref }) => (`<li
-        class="parsha"
-        data-target-id="parsha"
-        data-jump-to-book="${ref.b}"
-        data-jump-to-chapter="${ref.c}"
-        data-jump-to-verse="${ref.v}"
-      >${he}</li>`)).join('')}
+    <ol class="parsha-books">
+      ${parshiyot
+        .reduce((books, parsha) => {
+          const book = parsha.ref.b
+          books[book] = books[book] || []
+          books[book].push(parsha)
+          return books
+        }, [])
+        .map(Book)
+        .join('')
+      }
     </ol>
   </div>
 `

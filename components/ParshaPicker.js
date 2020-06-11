851080bff1be5634b5e81ac793dadf1f344bab2c
Akiva Gordon <akivag29@gmail.com>
2020-06-11 13:08:05 -0400

    Closes #61: Show "Coming up" parshiyot based on current date

--- components/ParshaPicker.js
+++ components/ParshaPicker.js
@@ -1,6 +1,7 @@
 /* global gtag */
 
 import parshiyot from '../build/parshiyot.json'
+import readingSchedule from '../build/schedule.json'
 import fuzzy from '../src/fuzzy'
 import utils from './utils'
 import ParshaResult, { NoResults } from './ParshaResult'
@@ -27,33 +28,75 @@ const Book = (book) => `
   </li>
 `
 
+const refFromLabel = ({ label }) => parshiyot
+  .find(({ he }) => label.startsWith(he))
+  .ref
+
+const ComingUpReading = ({ label, date, datetime }) => {
+  const { b: book, c: chapter, v: verse } = refFromLabel({ label })
+  return `
+    <li class="stack small" style="display: flex; flex-direction: column; align-items: center;">
+      <button
+        data-target-class="coming-up-reading"
+        data-jump-to-book="${book}"
+        data-jump-to-chapter="${chapter}"
+        data-jump-to-verse="${verse}"
+        data-scroll="torah"
+        style="background-color: hsla(0, 0%, 0%, 0.05); padding: 0.5em 1em; border-radius: 4px; color: hsla(0, 0%, 0%, 0.7); border: none; cursor: pointer;"
+      >${label}</button>
+      <time style="color: hsla(0, 0%, 0%, 0.7);" datetime="${datetime}">${date}</time>
+    </li>
+  `
+}
+
+const ComingUp = () => `
+  <section dir="ltr" id="coming-up" class="section mod-alternate mod-padding">
+    <div class="stack medium">
+      <label style="display: block; text-align: center; text-transform: uppercase; font-size: 0.8em; font-weight: 700; color: hsla(0, 0%, 0%, 0.7);">Coming up</label>
+      <ol class="cluster" style="list-style: none; display: flex; justify-content: center;">
+        ${readingSchedule
+          .filter(reading => new Date(reading.datetime) > new Date())
+          .slice(0, 3)
+          .map(({ label, date, datetime }) => ComingUpReading({ label, date, datetime }))
+          .join('')
+        }
+      </ol>
+    </div>
+  </section>
+`
+
 const ParshaPicker = (search, searchEmitter, jumpToRef) => {
   const self = htmlToElement(`
     <div class="parsha-picker">
-      <div id="search"></div>
-      <div class="browse">
-        <h2 class="section-heading">תורה</h2>
-        <ol class="parsha-books">
-          ${parshiyot
-            .reduce((books, parsha) => {
-              const book = parsha.ref.b
-              books[book] = books[book] || []
-              books[book].push(parsha)
-              return books
-            }, [])
-            .map(Book)
-            .join('')
-          }
-        </ol>
+      <div class="stack xlarge">
+        <div class="centerize">
+          <div id="search" style="display: inline-block;"></div>
+        </div>
+        ${ComingUp()}
+        <div class="browse">
+          <h2 class="section-heading">תורה</h2>
+          <ol class="parsha-books">
+            ${parshiyot
+              .reduce((books, parsha) => {
+                const book = parsha.ref.b
+                books[book] = books[book] || []
+                books[book].push(parsha)
+                return books
+              }, [])
+              .map(Book)
+              .join('')
+            }
+          </ol>
 
-        <h2 class="section-heading">מגילות</h2>
-        <ol class="parsha-books">
-          <li class="parsha-book">
-            <ol class="parsha-list">
-              ${Parsha({ ref: { b: 1, c: 1, v: 1 }, he: 'אסתר', scroll: 'esther' })}
-            </ol>
-          </li>
-        </ol>
+          <h2 class="section-heading">מגילות</h2>
+          <ol class="parsha-books">
+            <li class="parsha-book">
+              <ol class="parsha-list">
+                ${Parsha({ ref: { b: 1, c: 1, v: 1 }, he: 'אסתר', scroll: 'esther' })}
+              </ol>
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   `)
@@ -71,6 +114,7 @@ const ParshaPicker = (search, searchEmitter, jumpToRef) => {
 
   searchEmitter.on('search', query => {
     self.querySelector('.browse').classList.add('u-hidden')
+    self.querySelector('#coming-up').classList.add('u-hidden')
     gtag('event', 'search', {
       event_category: 'navigation',
       event_label: query
@@ -79,9 +123,10 @@ const ParshaPicker = (search, searchEmitter, jumpToRef) => {
 
   searchEmitter.on('clear', () => {
     self.querySelector('.browse').classList.remove('u-hidden')
+    self.querySelector('#coming-up').classList.remove('u-hidden')
   })
 
-  self.replaceChild(search, self.querySelector('#search'))
+  self.querySelector('#search').parentNode.replaceChild(search, self.querySelector('#search'))
 
   ;[...self.querySelectorAll('[data-target-id="parsha"]')]
     .forEach((parsha) => {
@@ -90,6 +135,18 @@ const ParshaPicker = (search, searchEmitter, jumpToRef) => {
           event_category: 'navigation',
           event_label: e.target.textContent.trim()
         })
+        jumpToRef({ ref: e.target, scroll: e.target.getAttribute('data-scroll') })
+      })
+    })
+
+  ;[...self.querySelectorAll('[data-target-class="coming-up-reading"]')]
+    .forEach(comingUpReading => {
+      comingUpReading.addEventListener('click', e => {
+        gtag('event', 'coming_up_selection', {
+          event_category: 'navigation',
+          event_label: e.target.textContent.trim()
+        })
+
         jumpToRef({ ref: e.target, scroll: e.target.getAttribute('data-scroll') })
       })
     })

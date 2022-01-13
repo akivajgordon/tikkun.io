86cb22925b37c95d6412b1cb4377e676d189b004
Akiva Gordon <akivag29@gmail.com>
2022-01-12 21:48:13 -0500

    Update source to fix a whole lot of issues

--- src/scrolls-by-key.js
+++ src/scrolls-by-key.js
@@ -2,16 +2,26 @@ import { IntegerIterator, physicalLocationFromRef, title as getTitle } from '.'
 import pageTitles from '../build/page-titles.json'
 import holydays from '../build/holydays.json'
 
-const fetchPage = ({ path, title }) => window.fetch(path)
-  .then((res) => res.json())
-  .then((page) => ({ content: page, title }))
-  .catch((err) => {
-    console.error(err)
-  })
+const fetchPage = ({ path, title, pageNumber }) =>
+  window
+    .fetch(path)
+    .then((res) => res.json())
+    .then((page) => ({ content: page, title, pageNumber }))
+    .catch((err) => {
+      console.error(err)
+    })
 
 const Scroll = {
-  new: ({ scroll, makePath, makeTitle, startingAtRef = { b: 1, c: 1, v: 1 } }) => {
-    const { pageNumber, lineNumber } = physicalLocationFromRef({ ref: startingAtRef, scroll })
+  new: ({
+    scroll,
+    makePath,
+    makeTitle,
+    startingAtRef = { b: 1, c: 1, v: 1 },
+  }) => {
+    const { pageNumber, lineNumber } = physicalLocationFromRef({
+      ref: startingAtRef,
+      scroll,
+    })
 
     const iterator = IntegerIterator.new({ startingAt: pageNumber })
 
@@ -20,53 +30,61 @@ const Scroll = {
       fetchPrevious: () => {
         const n = iterator.previous()
         if (n <= 0) return Promise.resolve()
-        return fetchPage({ path: makePath(n), title: makeTitle(n) })
+        return fetchPage({
+          path: makePath(n),
+          title: makeTitle(n),
+          pageNumber: n,
+        })
       },
       fetchNext: () => {
         const n = iterator.next()
-        return fetchPage({ path: makePath(n), title: makeTitle(n) })
+        return fetchPage({
+          path: makePath(n),
+          title: makeTitle(n),
+          pageNumber: n,
+        })
       },
-      startingLineNumber: lineNumber
+      startingLineNumber: lineNumber,
     }
-  }
+  },
 }
 
 const TorahScroll = {
   new: ({ startingAtRef }) => {
     return Scroll.new({
       scroll: 'torah',
-      makePath: n => `/build/pages/torah/${n}.json`,
-      makeTitle: n => getTitle(pageTitles[n - 1]),
-      startingAtRef
+      makePath: (n) => `/build/pages/torah/${n}.json`,
+      makeTitle: (n) => getTitle(pageTitles[n - 1]),
+      startingAtRef,
     })
-  }
+  },
 }
 
 const EstherScroll = {
   new: ({ startingAtRef }) => {
     return Scroll.new({
       scroll: 'esther',
-      makePath: n => `/build/pages/esther/${n}.json`,
-      makeTitle: n => 'אסתר',
-      startingAtRef
+      makePath: (n) => `/build/pages/esther/${n}.json`,
+      makeTitle: (n) => 'אסתר',
+      startingAtRef,
     })
-  }
+  },
 }
 
 export default {
-  'torah': TorahScroll,
-  'esther': EstherScroll,
+  torah: TorahScroll,
+  esther: EstherScroll,
   ...Object.keys(holydays).reduce((result, holydayKey) => {
     const HolydayScroll = {
       new: ({ startingAtRef }) => {
         return Scroll.new({
           scroll: holydayKey,
-          makePath: n => `/build/pages/${holydayKey}/${n}.json`,
-          makeTitle: n => holydays[holydayKey].he,
-          startingAtRef
+          makePath: (n) => `/build/pages/${holydayKey}/${n}.json`,
+          makeTitle: (n) => holydays[holydayKey].he,
+          startingAtRef,
         })
-      }
+      },
     }
     return { ...result, [holydayKey]: HolydayScroll }
-  }, {})
+  }, {}),
 }

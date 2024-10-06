a5dc07873dc709a2f06ee32140191e5d406ab729
SLaks <Dev@SLaks.net>
2024-10-06 15:08:46 -0400

    Calendar: Remove time-zone-dependent dates from snapshots
    This makes tests pass on GitHub Actions, which set the timezone to UTC.

--- src/calendar-model/generator.test.ts
+++ src/calendar-model/generator.test.ts
@@ -2,6 +2,7 @@ import test from 'ava'
 import { UserSettings } from './user-settings.ts'
 import { LeiningGenerator } from './generator.ts'
 import { HDate, months } from '@hebcal/core'
+import { LeiningDate } from './model-types.ts'
 const testSettings: UserSettings = {
   ashkenazi: true,
   includeModernHolidays: false,
@@ -23,29 +24,53 @@ for (let year = 5780; year < 5800; year++) {
 }
 
 test('generates יום כיפור', (t) => {
-  t.snapshot(generator.createLeiningDate(new HDate(10, months.TISHREI, 5784)))
+  t.snapshot(
+    stripDate(generator.createLeiningDate(new HDate(10, months.TISHREI, 5784)))
+  )
 })
 
 test('generates שמחת תורה', (t) => {
-  t.snapshot(generator.createLeiningDate(new HDate(23, months.TISHREI, 5784)))
+  t.snapshot(
+    stripDate(generator.createLeiningDate(new HDate(23, months.TISHREI, 5784)))
+  )
 })
 
 test('generates תענית אסתר', (t) => {
-  t.snapshot(generator.createLeiningDate(new HDate(13, months.ADAR_II, 5785)))
+  t.snapshot(
+    stripDate(generator.createLeiningDate(new HDate(13, months.ADAR_II, 5785)))
+  )
 })
 
 test('generates פורים', (t) => {
-  t.snapshot(generator.createLeiningDate(new HDate(14, months.ADAR_II, 5784)))
+  t.snapshot(
+    stripDate(generator.createLeiningDate(new HDate(14, months.ADAR_II, 5784)))
+  )
 })
 
 test('generates ערב תשעה באב', (t) => {
-  t.snapshot(generator.createLeiningDate(new HDate(8, months.AV, 5784)))
+  t.snapshot(
+    stripDate(generator.createLeiningDate(new HDate(8, months.AV, 5784)))
+  )
 })
 
 test('generates תשעה באב', (t) => {
-  t.snapshot(generator.createLeiningDate(new HDate(9, months.AV, 5784)))
+  t.snapshot(
+    stripDate(generator.createLeiningDate(new HDate(9, months.AV, 5784)))
+  )
 })
 
 test('generates שבת ראש חודש חנוכה', (t) => {
-  t.snapshot(generator.createLeiningDate(new HDate(30, months.KISLEV, 5782)))
+  t.snapshot(
+    stripDate(generator.createLeiningDate(new HDate(30, months.KISLEV, 5782)))
+  )
 })
+
+/**
+ * Removes the `date` property, which is time-zone dependent.
+ * This makes the recorded snapshot consistent across timezones.
+ * You can see the date from the `id` property.
+ */
+function stripDate(o: LeiningDate): unknown {
+  delete o.date
+  return o
+}

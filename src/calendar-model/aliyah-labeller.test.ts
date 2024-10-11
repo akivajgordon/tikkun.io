18d1f9d331f04c70384369ee0e45c335ea142002
SLaks <Dev@SLaks.net>
2024-10-11 14:59:41 -0400

    Calendar: Add unit tests for עלייה labelling
    This also covers HolidayViewModel's run selection

---
+++ src/calendar-model/aliyah-labeller.test.ts
@@ -0,0 +1,93 @@
+/** @fileoverview This file tests both Aliyah labelling and run calculation in HolidayViewModel. */
+
+import test from 'ava'
+import { LeiningGenerator } from './generator.ts'
+import { UserSettings } from './user-settings.ts'
+import { RenderedEntry, ScrollViewModel } from './scroll-view-model.ts'
+
+const testSettings: UserSettings = {
+  ashkenazi: true,
+  includeModernHolidays: false,
+  israel: false,
+}
+
+const generator = new LeiningGenerator(testSettings)
+
+test(`יום כיפור on שבת`, async (t) => {
+  t.snapshot(await dumpAliyot('2024-10-12:shacharis,main'))
+})
+
+test(`חול המוד סוכות`, async (t) => {
+  t.snapshot(await dumpAliyot('2024-10-20:shacharis,main'))
+})
+
+test(`שמחת תורה`, async (t) => {
+  t.snapshot(await dumpAliyot('2024-10-25:shacharis,main'))
+})
+
+test(`תענית ציבור`, async (t) => {
+  t.snapshot(await dumpAliyot('2025-03-13:mincha,main'))
+})
+
+test(`פרשת האזינו`, async (t) => {
+  t.snapshot(await dumpAliyot('2024-10-05:shacharis,main'))
+})
+
+test(`שקלים / ראש חודש as פרשה`, async (t) => {
+  // Fetch the main run so that we get a FullScrollViewModel
+  t.snapshot(await dumpAliyot('2025-03-01:shacharis,main'))
+})
+
+test(`Weekday ראש חודש`, async (t) => {
+  t.snapshot(await dumpAliyot('2025-02-28:shacharis,main'))
+})
+
+test(`שקלים / ראש חודש as מפטיר`, async (t) => {
+  // Fetch a non-main run so that we get a HolidayViewModel
+  t.snapshot(await dumpAliyot('2025-03-01:shacharis,maftir'))
+})
+
+/** Formats the עלייה-labelled lines from a ScrollViewModel to read in the snapshot. */
+async function dumpAliyot(runId: string) {
+  const model = ScrollViewModel.forId(generator, runId)
+  if (!model) throw new Error(`ID ${runId} not found`)
+
+  const pages: RenderedEntry[] = [(await model.startingLocation).page]
+  if (pages[0].type !== 'page') throw new Error('First page should be a page')
+
+  const targetDate = generator.parseId(runId)!.leining.date
+
+  // If this is a HolidayViewModel, fetch the previous runs as well.
+  if (model.relevantRuns.length < 10) {
+    while (true) {
+      const previousPage = await model.fetchPreviousPage()
+      if (!previousPage) break
+      pages.unshift(previousPage)
+    }
+  }
+
+  while (true) {
+    const nextPage = await model.fetchNextPage()
+    if (!nextPage) break
+
+    if (nextPage.type === 'page') {
+      // If a page does not have a run, keep going
+      // (pass over the skipped page for תענית ציבור).
+      const nextPageRun = nextPage.run?.leining.date ?? targetDate
+      if (nextPageRun.date > targetDate.date) break
+    }
+    pages.push(nextPage)
+  }
+
+  return pages.flatMap((e) => {
+    if (e.type === 'message') return [e.text]
+    return e.lines
+      .filter((line) => line.labels.length)
+      .map(
+        (line) =>
+          `${line.labels}: ${line.text
+            .map((spans) => spans.join(' '))
+            .join('\t')}`
+      )
+  })
+}

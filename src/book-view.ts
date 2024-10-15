87f0213f86f1ad3942978682975081acd76ddef8
SLaks <Dev@SLaks.net>
2024-10-15 11:38:18 -0400

    UI: Start rewriting viewport tracker
    This is incomplete and does not work

--- src/book-view.ts
+++ src/book-view.ts
@@ -1,38 +1,67 @@
 import { EventEmitter } from './event-emitter.ts'
-import { RenderedLineInfo } from './view-model/scroll-view-model.ts'
+import {
+  RenderedLineInfo,
+  RenderedPageInfo,
+} from './view-model/scroll-view-model.ts'
+
+interface ViewportRange {
+  first: RenderedLineInfo | null
+  center: RenderedLineInfo | null
+  last: RenderedLineInfo | null
+}
+
+declare global {
+  interface Element {
+    tikkunPage?: RenderedPageInfo
+  }
+}
 
 type BookViewEvents = {
-  'viewport-updated': RenderedLineInfo[]
+  'viewport-updated': ViewportRange
 }
 
 export class BookView extends EventEmitter<BookViewEvents> {
-  private currentPage: Page
   private book: HTMLElement
+  private readonly lineTrackers: {
+    first: ViewportTracker
+    center: ViewportTracker
+    last: ViewportTracker
+  }
 
   constructor(book: HTMLElement) {
     super()
 
     this.book = book
-    this.updateCurrentPage()
+    this.lineTrackers = {
+      first: new ViewportTracker(book),
+      center: new ViewportTracker(book),
+      last: new ViewportTracker(book),
+    }
+    this.update()
 
     book.addEventListener(
       'scroll',
       throttle(() => {
-        this.updateCurrentPage()
+        this.update()
       }, 300)
     )
   }
 
-  private updateCurrentPage() {
-    const pageAtCenter = this.pageAtCenter(this.book)
+  private update() {
+    let updated = false
 
-    if (!pageAtCenter) return
+    // We cannot use ||  because we always need to update all trackers.
+    const height = document.documentElement.clientHeight
+    if (this.lineTrackers.first.update(0)) updated = true
+    if (this.lineTrackers.center.update(height / 2)) updated = true
+    if (this.lineTrackers.last.update(height)) updated = true
 
-    const page = Page.from(pageAtCenter)
-
-    if (!this.currentPage || !this.currentPage.isPage(page)) {
-      this.emit('page-updated', page)
-      this.currentPage = page
+    if (updated) {
+      this.emit('viewport-updated', {
+        first: this.lineTrackers.first.current,
+        center: this.lineTrackers.center.current,
+        last: this.lineTrackers.last.current,
+      })
     }
   }
 
@@ -59,26 +88,57 @@ export class BookView extends EventEmitter<BookViewEvents> {
   }
 }
 
-class Page {
-  private id: string
-  title: string
+type KeysOfType<TObject, TType> = {
+  [TKey in keyof TObject]: TObject[TKey] extends TType ? TKey : never
+}[keyof TObject]
 
-  constructor({ id, title }: { id: string; title: string }) {
-    this.id = id
-    this.title = title
+const ElementSearchDirection = {
+  Down: { coordinate: 'top', nextElement: 'nextNode' },
+  Up: { coordinate: 'bottom', nextElement: 'previousNode' },
+} satisfies Record<
+  string,
+  {
+    coordinate: keyof DOMRect
+    nextElement: KeysOfType<TreeWalker, () => Node | null>
+  }
+>
+
+/**
+ * Efficiently locates the closest RenderedLineInfo to a point on the screen.
+ * Updates as the user scrolls.
+ */
+class ViewportTracker {
+  private readonly walker: TreeWalker
+  constructor(root: Element) {
+    this.walker = document.createTreeWalker(
+      root,
+      NodeFilter.SHOW_ELEMENT,
+      (el) => {
+        if (!(el instanceof HTMLElement)) return NodeFilter.FILTER_REJECT
+        if (el.tagName === 'TD') return NodeFilter.FILTER_REJECT
+        if (el.dataset.lineIndex) return NodeFilter.FILTER_ACCEPT
+        return NodeFilter.FILTER_SKIP
+      }
+    )
   }
 
-  static from(p: Element): Page {
-    return new Page({
-      id: p.getAttribute('data-page-number'),
-      title: p.getAttribute('data-page-title'),
-    })
+  get current() {
+    return getLineInfo(this.walker.currentNode)
   }
 
-  isPage(other: Page): boolean {
-    return this.id === other.id
+  /** Updates the current element.  Returns false if it did not change. */
+  update(targetY: number): boolean {
+    // TODO: Actually locate, then track, walker.currentNode.
+    return true
   }
 }
+
+function getLineInfo(el: Node) {
+  if (!(el instanceof HTMLElement)) return null
+  const pageNode = el.closest('.tikkun-page')
+  return pageNode?.tikkunPage?.lines[Number(el.dataset.lineIndex)] ?? null
+}
+
 function throttle<TReturn, TArgs extends unknown[]>(
   func: (...args: TArgs) => TReturn,
   limit: number
